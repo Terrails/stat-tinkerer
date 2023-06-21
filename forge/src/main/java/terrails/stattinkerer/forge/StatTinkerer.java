6e958819599be02880fb651b87edc0202ad66855
Terrails <github@sl.terrails.com>
2023-06-21 16:18:58 +0200

    Let Forge handle config loading - file in config folder makes no sense as everything should be configured server side and the client just should receive the data. This of course makes it so that singleplayer users will now have to access the config in serveconfig folder of each world. Something similar should probably be implemented on fabric and quilt by manually sending packets from server to client.

--- forge/src/main/java/terrails/stattinkerer/forge/StatTinkerer.java
+++ forge/src/main/java/terrails/stattinkerer/forge/StatTinkerer.java
@@ -1,8 +1,5 @@
 package terrails.stattinkerer.forge;
 
-import com.electronwill.nightconfig.core.file.CommentedFileConfig;
-import com.electronwill.nightconfig.core.io.ParsingException;
-import com.electronwill.nightconfig.core.io.WritingMode;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.common.capabilities.RegisterCapabilitiesEvent;
@@ -16,7 +13,6 @@ import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.fml.loading.FMLPaths;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -27,12 +23,7 @@ import terrails.stattinkerer.config.ConfigOption;
 import terrails.stattinkerer.forge.feature.TANFeature;
 import terrails.stattinkerer.forge.mobeffect.NoAppetiteMobEffect;
 
-import java.io.IOException;
 import java.lang.reflect.Field;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -48,73 +39,19 @@ public class StatTinkerer {
     private static final RegistryObject<MobEffect> NO_APPETITE = MOB_EFFECTS.register("no_appetite", NoAppetiteMobEffect::new);
 
     public StatTinkerer() {
-        ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, CONFIG_SPEC, CStatTinkerer.MOD_ID + ".toml");
+        ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, CONFIG_SPEC);
         final IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
         MOB_EFFECTS.register(bus);
         bus.addListener(this::setup);
     }
 
     private void setup(final FMLCommonSetupEvent event) {
-        StatTinkerer.loadConfig();
         STMobEffects.NO_APPETITE = NO_APPETITE.get();
     }
 
     @SubscribeEvent
     public static void registerCapabilities(final RegisterCapabilitiesEvent event) {
         event.register(HealthManager.class);
-    }
-
-    private static void loadConfig() {
-        final Path path = FMLPaths.CONFIGDIR.get().resolve(CStatTinkerer.MOD_ID + ".toml");
-
-        LOGGER.debug("Loading config file {}", path);
-
-        while (true) {
-            final CommentedFileConfig config = CommentedFileConfig.builder(path)
-                    .sync()
-                    .autoreload()
-                    .writingMode(WritingMode.REPLACE)
-                    .build();
-
-            try {
-                config.load();
-                LOGGER.debug("Loaded TOML config file {}", path.toString());
-                CONFIG_SPEC.setConfig(config);
-
-                // Initialize values from ConfigOption objects
-                for (Object object : CStatTinkerer.CONFIGURATION_INSTANCES) {
-                    for (Field field : object.getClass().getDeclaredFields()) {
-                        try {
-                            field.setAccessible(true);
-
-                            if (field.get(object) instanceof ConfigOption<?> option) {
-                                option.initialize(() -> config.get(option.getPath()), (val) -> config.set(option.getPath(), val));
-                            }
-
-                        } catch (Exception e) {
-                            LOGGER.error("Could not process spec for {} in {}", field.getName(), object.getClass().getName());
-                            e.printStackTrace();
-                        }
-                    }
-                }
-                break;
-            } catch (ParsingException e) {
-                config.close();
-
-                LOGGER.error("Failed to load '{}' due to a parsing error.", path.toString());
-                e.printStackTrace();
-
-                String deformedFile = (CStatTinkerer.MOD_ID + "-" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd_HH.mm.ss")) + ".toml");
-                try {
-                    Files.move(path, FMLPaths.CONFIGDIR.get().resolve(deformedFile));
-                    LOGGER.error("Deformed config file renamed to '{}'", deformedFile);
-                } catch (IOException ee) { // Results in an infinite loop, but considering that everything is broken without a config file, throw an exception and crash
-                    LOGGER.error("Moving deformed config file failed...");
-                    ee.printStackTrace();
-                    throw new RuntimeException("Could not initialize '%s' config file.".formatted(path.toString()));
-                }
-            }
-        }
     }
 
     static {
@@ -125,7 +62,7 @@ public class StatTinkerer {
                 try {
                     field.setAccessible(true);
 
-                    if (field.get(object) instanceof ConfigOption<?> option) {
+                    if (field.get(object) instanceof ConfigOption option) {
 
                         String comment = option.getComment().isEmpty() ? "" : option.getComment() + "\n";
                         if (option.getDefault() instanceof List<?> list) {
@@ -147,7 +84,8 @@ public class StatTinkerer {
                             comment += "Default: %s".formatted(option.getDefault().toString());
                         }
 
-                        builder.comment(comment).define(option.getPath(), option.getDefault(), option.getOptionValidator());
+                        ForgeConfigSpec.ConfigValue<Object> value = builder.comment(comment).define(option.getPath(), option.getDefault(), option.getOptionValidator());
+                        option.initialize(value::get, value::set);
                     }
 
                 } catch (Exception e) {
